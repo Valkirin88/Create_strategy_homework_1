3a6b7995940f1ddade9f4a2888163950d11379e6
Valkirin88 <valkirin@rambler.ru>
2022-09-30 13:24:45 +0300

    v0.5
    Both tasks are done

--- Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
+++ Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
@@ -7,7 +7,8 @@ public sealed class MouseInteractionPresenter : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private SelectableValue _selectedObject;
-    [SerializeField] private GameObject _selectableMarker;
+   
+   
 
     private void Update()
     {
@@ -24,7 +25,7 @@ public sealed class MouseInteractionPresenter : MonoBehaviour
             .Select(hit => hit.collider.GetComponentInParent<ISelectable>())
             .FirstOrDefault(c => c != null);
         _selectedObject.SetValue(selectable);
-       
-        Instantiate(_selectableMarker, new Vector3(Input.mousePosition.x, Input.mousePosition.y), Quaternion.identity);
+
+      
     }
 }
---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/SelectableMarker.cs
@@ -0,0 +1,36 @@
+using UserControlSystem;
+using UnityEngine;
+using System.Linq;
+using Abstractions;
+
+public sealed class SelectableMarker : MonoBehaviour
+{
+    [SerializeField] private GameObject _selectableMarker;
+    [SerializeField] private SelectableValue _selectedObject;
+    [SerializeField] private Camera _camera;
+    private Transform _selectableTransform;
+    private GameObject _marker;
+    
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonUp(0))
+        {
+            return;
+        }
+        var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
+        if (hits.Length == 0)
+        {
+            
+            return;
+        }
+        Destroy(_marker);
+        var selectable = hits
+            .Select(hit => hit.collider.GetComponentInParent<ISelectable>())
+            .FirstOrDefault(c => c != null);
+       if(selectable != null)
+        _selectableTransform = (selectable as Component).transform;
+       
+        _marker = Instantiate(_selectableMarker, _selectableTransform.position, Quaternion.identity);
+    }
+}
